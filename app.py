2210d57d2bdfc2200254a5846d64bf45320beff6
Gregorio Coronado Moron <gregorio.coronado@iescampanillas.com>
2022-01-19 06:04:44 -0500

    v 1.0

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from Funciones import esPrimo
+
 # Aqui desarrollo mi programa
+
+numero = int(input("Escribe un numero> "))
+resultado=esPrimo(numero)
+
+if resultado==True:
+    print(f"El numero {numero} es primo")
+else:
+    print(f"El numero {numero} NO es primo")
